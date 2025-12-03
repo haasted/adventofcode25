b96645c565fdc02b3ba0b7020b4d6a0505d3558a
Henrik Aasted <henrik.aasted@lego.com>
2025-12-03 14:48:15 +0100

    Part II

--- day01/main.go
+++ day01/main.go
@@ -10,37 +10,53 @@ func main() {
 	inputs := loadInput()
 
 	var (
-		zeroCount = 0
-		dial      = 50
+		zeroCount  = 0
+		zeroPasses = 0
+		dial       = 50
 	)
 	for _, instruction := range inputs {
-		dial = moveDial(dial, instruction)
+		var zerosPassed int
+		dial, zerosPassed = moveDial(dial, instruction)
 
 		if dial == 0 {
 			zeroCount++
 		}
+
+		zeroPasses += zerosPassed
 	}
 
-	print("Number of zeros: ", zeroCount, "\n")
+	println("Number of zeros: ", zeroCount)
+	println("Number of zeros passed: ", zeroPasses)
+	println("Sum: ", zeroPasses+zeroCount)
+
 }
 
-func moveDial(dial int, instruction string) int {
+func moveDial(dial int, instruction string) (int, int) {
+	zeroCount := 0
+
 	steps, _ := strconv.Atoi(instruction[1:])
 	if instruction[0] == 'R' {
 		dial += steps
 	} else {
+		if dial == 0 {
+			zeroCount = -1
+		}
 		dial -= steps
 	}
 
-	if dial >= 100 {
-		dial = dial % 100
+	for dial >= 100 {
+		dial = dial - 100
+		if dial != 0 {
+			zeroCount++
+		}
 	}
 
 	for dial < 0 {
+		zeroCount++
 		dial = dial + 100
 	}
 
-	return dial
+	return dial, zeroCount
 }
 
 func loadInput() (inputs []string) {
--- day01/main_test.go
+++ day01/main_test.go
@@ -20,18 +20,44 @@ R14
 L82
 `
 
-func Test1(t *testing.T) {
+func TestPart1(t *testing.T) {
 	var (
-		dialValues = []int{82, 52, 0, 95, 55, 0, 99, 0, 14, 32}
-		dial       = 50
+		dialValues         = []int{82, 52, 0, 95, 55, 0, 99, 0, 14, 32}
+		expectedZeroCounts = []int{1, 0, 0, 0, 1, 0, 0, 0, 0, 1}
+		dial               = 50
+		zeroCount          int
+		zeroPasses         int
 	)
 
 	scanner := bufio.NewScanner(strings.NewReader(inputs))
 	for scanner.Scan() {
-		dial = moveDial(dial, scanner.Text())
+		var passes int
+		dial, passes = moveDial(dial, scanner.Text())
+
+		zeroPasses += passes
 		if dialValues[0] != dial {
 			panic(fmt.Sprint(dialValues[0], "!=", dial))
 		}
+		if expectedZeroCounts[0] != passes {
+			println("dial value ", dial, passes, fmt.Sprintf(" %+v - %+v", dialValues, expectedZeroCounts))
+			panic("unexpected zero count")
+		}
+
+		if dial == 0 {
+			zeroCount++
+		}
+
 		dialValues = dialValues[1:]
+		expectedZeroCounts = expectedZeroCounts[1:]
+	}
+
+	if zeroCount != 3 {
+		println(zeroCount)
+		panic("Unexpected amount of exact zeros")
+	}
+
+	if zeroCount+zeroPasses != 6 {
+		println(zeroCount)
+		panic("Unexpected amount of zero passes")
 	}
 }
