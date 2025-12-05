d9e545fad491d4844c889f40f15c1ba21cbcb90b
Henrik Aasted <henrik.aasted@lego.com>
2025-12-05 14:27:34 +0100

    Day 5, part 1

--- day05/main.go
+++ day05/main.go
@@ -1,5 +1,86 @@
 package main
 
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	start, end int
+}
+
+func (r Range) isInRange(val int) bool {
+	return r.start <= val && val <= r.end
+}
+
+type inventorySystem struct {
+	ranges []Range
+}
+
+func (is *inventorySystem) addRange(r string) {
+	is.ranges = append(is.ranges, newRange(r))
+}
+
+func (is *inventorySystem) isFresh(ingredient int) bool {
+	for _, r := range is.ranges {
+		if r.isInRange(ingredient) {
+			return true
+		}
+	}
+	return false
+}
+
+func newRange(r string) (res Range) {
+	r = strings.TrimSpace(r)
+	vals := strings.Split(r, "-")
+
+	res.start, _ = strconv.Atoi(vals[0])
+	res.end, _ = strconv.Atoi(vals[1])
+	return
+}
+
 func main() {
-	println("Hello welt")
+	is, ingredients := loadFromInputFile()
+	println("Ranges", len(is.ranges))
+	println("Ingredients", len(ingredients))
+
+	freshcount := 0
+	for _, ingredient := range ingredients {
+		if is.isFresh(ingredient) {
+			freshcount++
+		}
+	}
+
+	println("Number of fresh ingredients", freshcount)
+}
+
+func loadFromInputFile() (inventorySystem, []int) {
+	file, _ := os.Open("./input")
+	defer file.Close()
+	return loadInput(file)
+}
+
+func loadInput(reader io.Reader) (is inventorySystem, ingredients []int) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if txt == "" {
+			break
+		}
+
+		is.addRange(txt)
+	}
+
+	for scanner.Scan() {
+		i, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		ingredients = append(ingredients, i)
+	}
+
+	return
 }
--- day05/main_test.go
+++ day05/main_test.go
@@ -1,7 +1,39 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Test(t *testing.T) {
-	println("testing")
+	input := `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+`
+
+	is, ingredients := loadInput(strings.NewReader(input))
+	assert(len(is.ranges) == 4)
+	assert(len(ingredients) == 6)
+
+	for _, ingredient := range []int{1, 8, 32} {
+		assert(!is.isFresh(ingredient))
+	}
+
+	for _, ingredient := range []int{5, 11, 17} {
+		assert(is.isFresh(ingredient))
+	}
+}
+
+func assert(in bool) {
+	if !in {
+		panic("assertion failed")
+	}
 }
